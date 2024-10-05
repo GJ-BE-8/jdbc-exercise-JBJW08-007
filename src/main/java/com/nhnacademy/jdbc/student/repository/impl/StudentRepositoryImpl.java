659cb4e6b340f97a01a8d4c3ca8f244b4d5d7e91
nhn-academy-marco <marco@ nhnacademy.com>
2024-10-05 11:05:47 +0900

    exercise-student-crud-dbcp2 완료

--- src/main/java/com/nhnacademy/jdbc/student/repository/impl/StudentRepositoryImpl.java
+++ src/main/java/com/nhnacademy/jdbc/student/repository/impl/StudentRepositoryImpl.java
@@ -12,12 +12,57 @@ public class StudentRepositoryImpl implements StudentRepository {
     @Override
     public int save(Connection connection, Student student){
         //todo#2 학생등록
+
+        String query = "INSERT INTO jdbc_students(id, name, gender, age) VALUES(?,?,?,?)";
+
+        try(
+                PreparedStatement preparedStatement = connection.prepareStatement(query);
+                ){
+
+            preparedStatement.setString(1, student.getId());
+            preparedStatement.setString(2, student.getName());
+            preparedStatement.setString(3, student.getGender().name());
+            preparedStatement.setInt(4, student.getAge());
+
+            return preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            log.error("sql exception : {}", e);
+        }
         return 0;
     }
 
     @Override
     public Optional<Student> findById(Connection connection,String id){
         //todo#3 학생조회
+
+        String query = "SELECT * FROM jdbc_students WHERE id = ?";
+
+        try(
+                PreparedStatement preparedStatement = connection.prepareStatement(query);
+        ){
+
+            preparedStatement.setString(1, id);
+
+            try(ResultSet resultSet = preparedStatement.executeQuery()){
+                if (resultSet.next()){
+                    Student student = new Student(
+                            resultSet.getString("id"),
+                            resultSet.getString("name"),
+                            Student.GENDER.valueOf(resultSet.getString("gender")),
+                            resultSet.getInt("age"),
+                            resultSet.getTimestamp("created_at").toLocalDateTime()
+                    );
+
+                    return Optional.of(student);
+                }
+
+            }
+
+
+        } catch (SQLException e) {
+            log.error("sql exception : {}", e);
+        }
 
         return Optional.empty();
     }
@@ -26,12 +71,45 @@ public class StudentRepositoryImpl implements StudentRepository {
     public int update(Connection connection,Student student){
         //todo#4 학생수정
 
+        String query = "UPDATE jdbc_students SET name = ? , age = ? , gender = ? WHERE id = ?";
+
+        try(
+                PreparedStatement preparedStatement = connection.prepareStatement(query);
+        ){
+
+            preparedStatement.setString(1, student.getName());
+            preparedStatement.setString(3, student.getGender().name());
+            preparedStatement.setInt(2, student.getAge());
+            preparedStatement.setString(4, student.getId());
+
+            return preparedStatement.executeUpdate();
+
+
+        } catch (SQLException e) {
+            log.error("sql exception : {}", e);
+        }
+
         return 0;
     }
 
     @Override
     public int deleteById(Connection connection,String id){
         //todo#5 학생삭제
+
+        String query = "DELETE FROM jdbc_students WHERE id = ?";
+
+        try(
+                PreparedStatement preparedStatement = connection.prepareStatement(query);
+        ){
+
+            preparedStatement.setString(1, id);
+
+            return preparedStatement.executeUpdate();
+
+
+        } catch (SQLException e) {
+            log.error("sql exception : {}", e);
+        }
 
         return 0;
     }
