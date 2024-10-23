faf089afdd4fe61d23c96c3c9db3912d23a30a2c
gyeongmin2121@gmail.com <gyeongmin2121@gmail.com>
2024-10-23 15:52:19 +0900

    connection-driver-manager 완료

--- src/main/java/com/nhnacademy/jdbc/util/DbUtils.java
+++ src/main/java/com/nhnacademy/jdbc/util/DbUtils.java
@@ -13,7 +13,7 @@ public class DbUtils {
         Connection connection = null;
         try {
             //todo connection.
-            connection = DriverManager.getConnection("jdbc:mysql://133.186.241.167:3306/nhn_academy_0","nhn_academy_0","1j0OTaJgkS1@$dWl");
+            connection = DriverManager.getConnection("jdbc:mysql://133.186.241.167:3306/nhn_academy_7","nhn_academy_7","YtN@9E2BXmq1#$SD");
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
