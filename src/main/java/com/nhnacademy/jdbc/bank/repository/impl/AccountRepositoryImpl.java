baf775697eea07920d172651172aac570a37d50b
gyeongmin2121@gmail.com <gyeongmin2121@gmail.com>
2024-10-24 19:23:31 +0900

    exercise-transaction-bacnk-account

--- src/main/java/com/nhnacademy/jdbc/bank/repository/impl/AccountRepositoryImpl.java
+++ src/main/java/com/nhnacademy/jdbc/bank/repository/impl/AccountRepositoryImpl.java
@@ -2,6 +2,7 @@ package com.nhnacademy.jdbc.bank.repository.impl;
 
 import com.nhnacademy.jdbc.bank.domain.Account;
 import com.nhnacademy.jdbc.bank.repository.AccountRepository;
+import lombok.extern.slf4j.Slf4j;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -9,23 +10,70 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Optional;
 
+@Slf4j
 public class AccountRepositoryImpl implements AccountRepository {
 
     public Optional<Account> findByAccountNumber(Connection connection, long accountNumber){
         //todo#1 계좌-조회
+        String query = "SELECT * FROM jdbc_account WHERE account_number = ?";
+
+        try(PreparedStatement preparedStatement = connection.prepareStatement(query)){
+            preparedStatement.setLong(1, accountNumber);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()){
+                Account account = new Account(
+                        resultSet.getLong(1),
+                        resultSet.getString(2),
+                        resultSet.getLong(3)
+                );
+                return Optional.of(account);
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
         return Optional.empty();
     }
 
     @Override
     public int save(Connection connection, Account account) {
         //todo#2 계좌-등록, executeUpdate() 결과를 반환 합니다.
-        return 0;
+        String query = "INSERT INTO jdbc_account(account_number, name, balance) VALUES(?, ?, ?)";
+
+        try(PreparedStatement preparedStatement = connection.prepareStatement(query)){
+
+            preparedStatement.setLong(1, account.getAccountNumber());
+            preparedStatement.setString(2, account.getName());
+            preparedStatement.setLong(3, account.getBalance());
+
+            return preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public int countByAccountNumber(Connection connection, long accountNumber){
         int count=0;
         //todo#3 select count(*)를 이용해서 계좌의 개수를 count해서 반환
+        String query = "SELECT COUNT(*) FROM jdbc_account WHERE account_number = ?";
+
+        try(PreparedStatement preparedStatement = connection.prepareStatement(query)){
+            preparedStatement.setLong(1, accountNumber);
+
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    count = resultSet.getInt(1); // 첫 번째 열의 값을 가져옵니다.
+                }
+            }
+
+        } catch (SQLException e) {
+            log.error("sql exception : {}", e.getMessage());
+        }
 
         return count;
     }
@@ -33,6 +81,16 @@ public class AccountRepositoryImpl implements AccountRepository {
     @Override
     public int deposit(Connection connection, long accountNumber, long amount){
         //todo#4 입금, executeUpdate() 결과를 반환 합니다.
+        String query = "UPDATE jdbc_account SET balance = balance + ? WHERE account_number = ?";
+
+        try(PreparedStatement preparedStatement = connection.prepareStatement(query)){
+            preparedStatement.setLong(1,amount);
+            preparedStatement.setLong(2,accountNumber);
+
+            return preparedStatement.executeUpdate();
+        }catch (SQLException e) {
+            log.error("sql exception : {}", e.getMessage());
+        }
 
         return 0;
     }
@@ -40,12 +98,30 @@ public class AccountRepositoryImpl implements AccountRepository {
     @Override
     public int withdraw(Connection connection, long accountNumber, long amount){
         //todo#5 출금, executeUpdate() 결과를 반환 합니다.
+        String query = "UPDATE jdbc_account SET balance = balance - ? WHERE account_number = ?";
+
+        try(PreparedStatement preparedStatement = connection.prepareStatement(query)){
+            preparedStatement.setLong(1,amount);
+            preparedStatement.setLong(2,accountNumber);
+
+            return preparedStatement.executeUpdate();
+        }catch (SQLException e) {
+            log.error("sql exception : {}", e.getMessage());
+        }
         return 0;
     }
 
     @Override
     public int deleteByAccountNumber(Connection connection, long accountNumber) {
         //todo#6 계좌 삭제, executeUpdate() 결과를 반환 합니다.
+        String query = "DELETE FROM jdbc_account WHERE account_number = ?";
+
+        try(PreparedStatement preparedStatement = connection.prepareStatement(query)){
+            preparedStatement.setLong(1,accountNumber);
+            return preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            log.error("sql exception : {}", e.getMessage());
+        }
         return 0;
     }
 }
--- src/main/java/com/nhnacademy/jdbc/bank/service/impl/BankServiceImpl.java
+++ src/main/java/com/nhnacademy/jdbc/bank/service/impl/BankServiceImpl.java
@@ -8,6 +8,8 @@ import com.nhnacademy.jdbc.bank.repository.AccountRepository;
 import com.nhnacademy.jdbc.bank.repository.impl.AccountRepositoryImpl;
 import com.nhnacademy.jdbc.bank.service.BankService;
 
+import javax.security.auth.login.AccountExpiredException;
+import javax.swing.text.html.Option;
 import java.sql.Connection;
 import java.util.Optional;
 
@@ -23,45 +25,123 @@ public class BankServiceImpl implements BankService {
     public Account getAccount(Connection connection, long accountNumber){
         //todo#11 계좌-조회
 
-       return null;
+        Optional<Account> account = accountRepository.findByAccountNumber(connection, accountNumber);
+
+        if (account.isEmpty()){
+            throw new AccountNotFoundException(accountNumber);
+        }
+
+       return account.get();
     }
 
     @Override
     public void createAccount(Connection connection, Account account){
         //todo#12 계좌-등록
 
+        if (isExistAccount(connection, account.getAccountNumber())){
+            throw new AccountAreadyExistException(account.getAccountNumber());
+        }
+
+        int result = accountRepository.save(connection, account);
+
+        if(result<1){
+            throw new RuntimeException("DO not save account");
+        }
+
     }
 
     @Override
     public boolean depositAccount(Connection connection, long accountNumber, long amount){
         //todo#13 예금, 계좌가 존재하는지 체크 -> 예금실행 -> 성공 true, 실패 false;
+        if (!isExistAccount(connection, accountNumber)){
+           throw new AccountNotFoundException(accountNumber);
+        }
 
-        return false;
+        int count = accountRepository.deposit(connection, accountNumber, amount);
+        return count > 0;
     }
 
     @Override
     public boolean withdrawAccount(Connection connection, long accountNumber, long amount){
         //todo#14 출금, 계좌가 존재하는지 체크 ->  출금가능여부 체크 -> 출금실행, 성공 true, 실폐 false 반환
+        if (!isExistAccount(connection, accountNumber)){
+            throw new AccountNotFoundException(accountNumber);
+        }
 
-        return false;
+        Optional<Account> account_optional = accountRepository.findByAccountNumber(connection, accountNumber);
+
+        Account account = account_optional.get();
+
+        if (!account.isWithdraw(amount)){
+            throw new BalanceNotEnoughException(accountNumber);
+        }
+
+
+        int count = accountRepository.withdraw(connection, accountNumber, amount);
+        return count > 0;
     }
 
     @Override
     public void transferAmount(Connection connection, long accountNumberFrom, long accountNumberTo, long amount){
         //todo#15 계좌 이체 accountNumberFrom -> accountNumberTo 으로 amount만큼 이체
+        if (!isExistAccount(connection, accountNumberFrom)){
+            throw new AccountNotFoundException(accountNumberFrom);
+        }
+
+        if (!isExistAccount(connection, accountNumberTo)){
+            throw new AccountNotFoundException(accountNumberTo);
+        }
+
+        Optional<Account> accountFrom = accountRepository.findByAccountNumber(connection,accountNumberFrom);
+        if(accountFrom.isEmpty()){
+            throw new AccountNotFoundException(accountNumberFrom);
+        }
+
+        Optional<Account> accountTo = accountRepository.findByAccountNumber(connection,accountNumberTo);
+        if(accountTo.isEmpty()){
+            throw new AccountNotFoundException(accountNumberTo);
+        }
+
+
+
+        if(!accountFrom.get().isWithdraw(amount)){
+            throw new BalanceNotEnoughException(accountNumberFrom);
+        }
+
+        int result1 = accountRepository.withdraw(connection,accountNumberFrom,amount);
+
+        if(result1<1){
+            throw new RuntimeException("fail - withdraw :" + accountNumberFrom );
+        }
+
+        int result2 = accountRepository.deposit(connection,accountNumberTo,amount);
+
+        if(result2 <1){
+            throw new RuntimeException("fail - deposit : " + accountNumberTo);
+        }
+
 
     }
 
     @Override
     public boolean isExistAccount(Connection connection, long accountNumber){
         //todo#16 Account가 존재하면 true , 존재하지 않다면 false
-
-        return false;
+        int count = accountRepository.countByAccountNumber(connection, accountNumber);
+        return count>0;
     }
 
     @Override
     public void dropAccount(Connection connection, long accountNumber) {
         //todo#17 account 삭제
+        if (!isExistAccount(connection, accountNumber)){
+            throw new AccountNotFoundException(accountNumber);
+        }
+
+        int result = accountRepository.deleteByAccountNumber(connection, accountNumber);
+
+        if(result<1){
+            throw new RuntimeException("fail-dropAccount:" + accountNumber);
+        }
         
     }
 
